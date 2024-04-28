37723a5323a12c6bf7d5c182b97b771eda82fcce
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-28 21:40:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
-    //Write your code here
-
-}
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var firstName = document.querySelector('#firstName').value;
+    var lastName = document.querySelector('#lastName').value;
+    
+    alert(firstName + ' ' + lastName);
+});
